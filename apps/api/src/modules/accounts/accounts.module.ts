88b3545bf72af16cfb5db45c057d1f15ba8b2c07
John Leward Escote <jleward.escote17@gmail.com>
2026-07-02 14:56:19 +0800

    fix(api): wire audit module into accounts module

--- apps/api/src/modules/accounts/accounts.module.ts
+++ apps/api/src/modules/accounts/accounts.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { AuditModule } from '../../shared/audit/audit.module';
 import { DatabaseModule } from '../../shared/database/database.module';
 import { AuthModule } from '../auth/auth.module';
 import { AccountsPayableController } from './api/accounts-payable.controller';
@@ -7,7 +8,7 @@ import { AccountsPayableService } from './application/accounts-payable.service';
 import { ACCOUNTS_PROVIDERS } from './accounts.providers';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [AuditModule, AuthModule, DatabaseModule],
   controllers: [AccountsPayableController],
   providers: [AccountsPayableService, ...ACCOUNTS_PROVIDERS],
   exports: [AccountsPayableService, ...ACCOUNTS_PROVIDERS],
